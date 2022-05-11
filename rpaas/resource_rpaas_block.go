cdd8bc42f078d4bce20a1acfd7395c86d39c32f3
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-05-11 17:03:40 -0300

    fix and refactor rpaas_block

--- rpaas/resource_rpaas_block.go
+++ rpaas/resource_rpaas_block.go
@@ -6,7 +6,9 @@ package rpaas
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -14,6 +16,8 @@ import (
 
 	rpaas_client "github.com/tsuru/rpaas-operator/pkg/rpaas/client"
 )
+
+var validBlocks = []string{"root", "http", "server", "lua-server", "lua-worker"}
 
 func resourceRpaasBlock() *schema.Resource {
 	return &schema.Resource{
@@ -43,15 +47,15 @@ func resourceRpaasBlock() *schema.Resource {
 				Required: true,
 				ValidateDiagFunc: func(value interface{}, path cty.Path) diag.Diagnostics {
 					v := value.(string)
-					validBlocks := []string{"root", "http", "server", "lua-server", "lua-worker"}
+
 					for _, b := range validBlocks {
 						if b == v {
 							return nil
 						}
 					}
-					return diag.Errorf("Unexpected block name value: %s", v)
+					return diag.Errorf("Unexpected block name value %q. Allowed values: %v", v, validBlocks)
 				},
-				Description: "Name of the block that will receive the custom configuration content",
+				Description: fmt.Sprintf("Name of the block that will receive the custom configuration content. Allowed values: %v", validBlocks),
 			},
 			"content": {
 				Type:        schema.TypeString,
@@ -88,16 +92,18 @@ func resourceRpaasBlockCreate(ctx context.Context, d *schema.ResourceData, meta 
 		return diag.Errorf("Unable to create/update block %s for instance %s: %v", blockName, instance, err)
 	}
 
-	d.SetId(fmt.Sprintf("%s/%s", serviceName, instance))
+	d.SetId(fmt.Sprintf("%s/%s/%s", serviceName, instance, blockName))
 	return nil
 }
 
 func resourceRpaasBlockRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	provider := meta.(*rpaasProvider)
 
-	instance := d.Get("instance").(string)
-	serviceName := d.Get("service_name").(string)
-	blockName := d.Get("name").(string)
+	serviceName, instance, blockName, err := parseRpaasBlockID(d.Id())
+	d.Set("instance", instance)
+	d.Set("service_name", serviceName)
+	d.Set("name", blockName)
+
 	rpaasClient, err := provider.RpaasClient.SetService(serviceName)
 	if err != nil {
 		return diag.Errorf("Unable to create client for service %s: %v", serviceName, err)
@@ -106,6 +112,18 @@ func resourceRpaasBlockRead(ctx context.Context, d *schema.ResourceData, meta in
 	blocks, err := rpaasClient.ListBlocks(ctx, rpaas_client.ListBlocksArgs{Instance: instance})
 	if err != nil {
 		return diag.Errorf("Unable to get block %s for instance %s: %v", blockName, instance, err)
+	}
+
+	if blockName == "" {
+		// auto-fix old buggy ID
+		if len(blocks) > 1 {
+			return diag.Errorf("This resource was created with a old buggy version of the provider. There are multiple blocks and it is not possible to figure out which one should be used. You must resolve it manually")
+		}
+		b := blocks[0]
+		d.Set("name", b.Name)
+		d.Set("content", b.Content)
+		d.SetId(fmt.Sprintf("%s/%s/%s", serviceName, instance, b.Name))
+		return nil
 	}
 
 	for _, b := range blocks {
@@ -117,7 +135,6 @@ func resourceRpaasBlockRead(ctx context.Context, d *schema.ResourceData, meta in
 	}
 
 	d.SetId("")
-
 	return nil
 }
 
@@ -143,4 +160,22 @@ func resourceRpaasBlockDelete(ctx context.Context, d *schema.ResourceData, meta 
 		return diag.Errorf("Unable to remove block for instance %s: %v", instance, err)
 	}
 	return nil
+}
+
+func parseRpaasBlockID(id string) (serviceName, instance, blockName string, err error) {
+	splitID := strings.Split(id, "/")
+
+	// support old bugging ID
+	if len(splitID) < 2 || len(splitID) > 3 {
+		err = errors.New("Resource ID could not be parsed. Format should be \"service/instance/block\"")
+		return
+	}
+
+	serviceName = splitID[0]
+	instance = splitID[1]
+	if len(splitID) == 3 {
+		blockName = splitID[2]
+	}
+
+	return
 }
--- rpaas/resource_rpaas_block_test.go
+++ rpaas/resource_rpaas_block_test.go
@@ -20,21 +20,79 @@ import (
 )
 
 func TestAccRpaasBlock_basic(t *testing.T) {
+	setupFakeServerRpaasBlock(t)
+
+	resourceName := "rpaas_block.custom_block_server"
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		IDRefreshName:     resourceName,
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRpaasBlockConfig_basic("my_rpaas"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccResourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "instance", "my_rpaas"),
+					resource.TestCheckResourceAttr(resourceName, "service_name", "rpaasv2-be"),
+					resource.TestCheckResourceAttr(resourceName, "name", "server"),
+				),
+			},
+			// {
+			// 	// Testing Import
+			// 	Config:        `resource "rpaas_block" "imported" {}`,
+			// 	ResourceName:  "rpaas_block.imported",
+			// 	ImportStateId: "rpaasv2-be/my_rpaas/lua-worker",
+			// 	ImportState:   true,
+			// 	ImportStateCheck: func(s []*terraform.InstanceState) error {
+			// 		state := s[0]
+			// 		assert.Equal(t, "rpaasv2-be", state.Attributes["service_name"])
+			// 		assert.Equal(t, "my_rpaas", state.Attributes["instance"])
+			// 		assert.Equal(t, "lua-worker", state.Attributes["name"])
+			// 		assert.Equal(t, "imported", state.Attributes["content"])
+			// 		return nil
+			// 	},
+			// },
+		},
+	})
+}
+
+func testAccRpaasBlockConfig_basic(name string) string {
+	return fmt.Sprintf(`
+resource "rpaas_block" "custom_block_server" {
+	instance = "%s"
+	service_name = "rpaasv2-be"
+
+	name = "server"
+
+	content = <<-EOF
+	# nginx config
+	EOF
+}
+`, name)
+}
+
+type blocksServerResponse struct {
+	Blocks []types.Block `json:"blocks"`
+}
+
+func setupFakeServerRpaasBlock(t *testing.T) {
 	fakeServer := echo.New()
 	fakeServer.POST("/services/rpaasv2-be/proxy/my_rpaas", func(c echo.Context) error {
 		var p types.Block
 		err := c.Bind(&p)
 		require.NoError(t, err)
 		assert.Equal(t, "server", p.Name)
-		assert.Equal(t, "	# nginx config\n", p.Content)
+		assert.Equal(t, "# nginx config\n", p.Content)
 		return c.JSON(http.StatusOK, nil)
 	})
 	fakeServer.GET("/services/rpaasv2-be/proxy/my_rpaas", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct {
-			Blocks []types.Block `json:"blocks"`
-		}{
+		// qparam := c.Request().URL.Query()
+		// path := qparam["callback"][0]
+		// if path == "/resources/my_rpaas/files/custom_file.txt" {
+
+		return c.JSON(http.StatusOK, blocksServerResponse{
 			Blocks: []types.Block{
-				{Name: "server", Content: "	# nginx config\n"},
+				{Name: "server", Content: "# nginx config\n"},
 			},
 		})
 	})
@@ -47,38 +105,4 @@ func TestAccRpaasBlock_basic(t *testing.T) {
 	server := httptest.NewServer(fakeServer)
 	os.Setenv("TSURU_TARGET", server.URL)
 	os.Setenv("TSURU_TOKEN", "asdf")
-
-	resourceName := "rpaas_block.custom_block_server"
-	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		IDRefreshName:     resourceName,
-		ProviderFactories: testAccProviderFactories,
-		CheckDestroy:      nil,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccRpaasBlockConfig_basic(server.URL, "my_rpaas"),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccResourceExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "instance", "my_rpaas"),
-					resource.TestCheckResourceAttr(resourceName, "service_name", "rpaasv2-be"),
-					resource.TestCheckResourceAttr(resourceName, "name", "server"),
-				),
-			},
-		},
-	})
-}
-
-func testAccRpaasBlockConfig_basic(fakeServer, name string) string {
-	return fmt.Sprintf(`
-resource "rpaas_block" "custom_block_server" {
-	instance = "%s"
-	service_name = "rpaasv2-be"
-
-	name = "server"
-
-	content = <<EOF
-	# nginx config
-	EOF
-}
-`, name)
 }
