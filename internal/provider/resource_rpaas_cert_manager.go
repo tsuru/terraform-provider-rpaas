8b92c87ff3b2b8701f0f89391baafeabe9c4e76c
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-08-18 11:25:33 -0300

    add tflogs to rpaas_cert_manager

--- internal/provider/resource_rpaas_cert_manager.go
+++ internal/provider/resource_rpaas_cert_manager.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
@@ -19,9 +20,9 @@ import (
 
 func resourceRpaasCertManager() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceRpaasCertManagerUpsert,
+		CreateContext: resourceRpaasCertManagerCreate,
 		ReadContext:   resourceRpaasCertManagerRead,
-		UpdateContext: resourceRpaasCertManagerUpsert,
+		UpdateContext: resourceRpaasCertManagerUpdate,
 		DeleteContext: resourceRpaasCertManagerDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -59,7 +60,7 @@ func resourceRpaasCertManager() *schema.Resource {
 	}
 }
 
-func resourceRpaasCertManagerUpsert(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceRpaasCertManagerCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	provider := meta.(*rpaasProvider)
 
 	serviceName := d.Get("service_name").(string)
@@ -69,9 +70,16 @@ func resourceRpaasCertManagerUpsert(ctx context.Context, d *schema.ResourceData,
 	}
 
 	instance, issuer, dnsNames := d.Get("instance").(string), d.Get("issuer").(string), asSliceOfStrings(d.Get("dns_names"))
-	err = rpaasRetry(ctx, d, func() error {
-		log.Printf("[DEBUG] Creating Cert Manager certificate request: {service: %s, instance: %s, issuer: %v, DNSes: %s}", serviceName, instance, issuer, strings.Join(dnsNames, ", "))
 
+	tflog.Info(ctx, "Create rpaas_cert_manager", map[string]interface{}{
+		"service":  serviceName,
+		"instance": instance,
+		"issuer":   issuer,
+		"dnsNames": dnsNames,
+	})
+
+	err = rpaasRetry(ctx, d, func() error {
+		// UpdateCertManager is really an upsert
 		return rpaasClient.UpdateCertManager(ctx, rpaas_client.UpdateCertManagerArgs{
 			Instance: instance,
 			CertManager: types.CertManager{
@@ -81,7 +89,7 @@ func resourceRpaasCertManagerUpsert(ctx context.Context, d *schema.ResourceData,
 		})
 	})
 	if err != nil {
-		return diag.Errorf("could not create/update Cert Manager request: %v", err)
+		return diag.Errorf("could not create Cert Manager request: %v", err)
 	}
 
 	d.SetId(fmt.Sprintf("%s::%s::%s", serviceName, instance, issuer))
@@ -123,6 +131,43 @@ func resourceRpaasCertManagerRead(ctx context.Context, d *schema.ResourceData, m
 	return nil
 }
 
+func resourceRpaasCertManagerUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	provider := meta.(*rpaasProvider)
+
+	serviceName, instance, issuer, err := parseCertManagerID(d.Id())
+	if err != nil {
+		return diag.Errorf("Unable to parse CertManager ID: %v", err)
+	}
+	rpaasClient, err := provider.RpaasClient.SetService(serviceName)
+	if err != nil {
+		return diag.Errorf("Unable to create client for service %s: %v", serviceName, err)
+	}
+
+	dnsNames := asSliceOfStrings(d.Get("dns_names"))
+
+	tflog.Info(ctx, "Update rpaas_cert_manager", map[string]interface{}{
+		"service":  serviceName,
+		"instance": instance,
+		"issuer":   issuer,
+		"dnsNames": dnsNames,
+	})
+
+	err = rpaasRetry(ctx, d, func() error {
+		return rpaasClient.UpdateCertManager(ctx, rpaas_client.UpdateCertManagerArgs{
+			Instance: instance,
+			CertManager: types.CertManager{
+				Issuer:   issuer,
+				DNSNames: dnsNames,
+			},
+		})
+	})
+	if err != nil {
+		return diag.Errorf("could not update Cert Manager request: %v", err)
+	}
+
+	return resourceRpaasCertManagerRead(ctx, d, meta)
+}
+
 func resourceRpaasCertManagerDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	provider := meta.(*rpaasProvider)
 
@@ -133,6 +178,13 @@ func resourceRpaasCertManagerDelete(ctx context.Context, d *schema.ResourceData,
 	}
 
 	instance, issuer := d.Get("instance").(string), d.Get("issuer").(string)
+
+	tflog.Info(ctx, "Delete rpaas_cert_manager", map[string]interface{}{
+		"service":  serviceName,
+		"instance": instance,
+		"issuer":   issuer,
+	})
+
 	err = rpaasRetry(ctx, d, func() error {
 		log.Printf("[DEBUG] Removing Cert Manager certificate request: {Service: %s, Instance: %s, Issuer: %s}", serviceName, instance, issuer)
 		return rpaasClient.DeleteCertManager(ctx, instance, issuer)
